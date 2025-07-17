fa47d981d849280a3193e5789686d9ffb6ec2a7b
Diogo Chaves <diogo.chaves@lcg.consulting>
2025-07-17 15:55:22 +0100

    Chapter 4

---
+++ chapter4/django_project/urls.py
@@ -0,0 +1,9 @@
+# django_project/urls.py
+from django.contrib import admin
+from django.urls import path, include # new
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("books.urls")), # new
+    path("api/", include("apis.urls")), # new
+]
